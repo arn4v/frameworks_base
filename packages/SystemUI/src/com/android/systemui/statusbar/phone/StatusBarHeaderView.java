99de356118035554c13c258ddd2ae9b3952752eb
d34d <clark@cyngn.com>
2016-01-14 10:44:38 -0800

    SysUI: Clean up objects when recreating statusbar
    Recreating the status bar for theme changes causes several views
    and objects to not get GCed.  By analyzing hprof dumps of
    com.android.systemui we can identify these issues and get things
    properly cleaned up, and that's what this patch is doing.
    
    Change-Id: Ieaec759e4378a9a7701e6fba46ed33464caaee0a
    TICKET: CYNGNOS-1041
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
    
    Conflicts:
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/KeyguardStatusBarView.java
    	packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBarHeaderView.java
@@ -173,6 +173,7 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
     private ImageView mBackgroundImage;
     private Drawable mCurrentBackground;
     private float mLastHeight;
+    private UserInfoController mUserInfoController;
 
     public StatusBarHeaderView(Context context, AttributeSet attrs) {
         super(context, attrs);
@@ -285,6 +286,15 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
 
         updateClockScale();
         updateClockCollapsedMargin();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        if (mUserInfoController != null) {
+            mUserInfoController.removeListener(mUserInfoChangedListener);
+        }
+        setListening(false);
     }
 
     private void updateClockCollapsedMargin() {
@@ -591,13 +601,17 @@ public class StatusBarHeaderView extends RelativeLayout implements View.OnClickL
         invalidateOutline();
     }
 
+    private UserInfoController.OnUserInfoChangedListener mUserInfoChangedListener =
+            new UserInfoController.OnUserInfoChangedListener() {
+        @Override
+        public void onUserInfoChanged(String name, Drawable picture) {
+            mMultiUserAvatar.setImageDrawable(picture);
+        }
+    };
+
     public void setUserInfoController(UserInfoController userInfoController) {
-        userInfoController.addListener(new UserInfoController.OnUserInfoChangedListener() {
-            @Override
-            public void onUserInfoChanged(String name, Drawable picture) {
-                mMultiUserAvatar.setImageDrawable(picture);
-            }
-        });
+        mUserInfoController = userInfoController;
+        userInfoController.addListener(mUserInfoChangedListener);
     }
 
     @Override
